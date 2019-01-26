30400716bf8904142695356a17fe71cd5635fc6b
John Wang <john.wang@uoit.net>
2019-01-26 13:14:38 -0500

    added movement logic for player and house

---
+++ Global Game Jam 2019/Assets/Scripts/Movement.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+	public float moveSpeed = 2.0f;
+
+	private GameObject player;
+	private Rigidbody2D playerRigid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		player = GameObject.FindGameObjectWithTag("Player");
+		playerRigid = player.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		
+    }
+
+	// Movement update
+	private void FixedUpdate()
+	{
+
+		//double inputs
+		if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A)) {
+			playerRigid.AddForce(new Vector2(-moveSpeed * 500.0f, moveSpeed * 500.0f));
+		}
+		else if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D)){
+			playerRigid.AddForce(new Vector2(moveSpeed * 500.0f, moveSpeed * 500.0f));
+		}
+		else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A)){
+			playerRigid.AddForce(new Vector2(-moveSpeed * 500.0f, -moveSpeed * 500.0f));
+		}
+		else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D)){
+			playerRigid.AddForce(new Vector2(moveSpeed * 500.0f, -moveSpeed * 500.0f));
+		}
+		else
+		{
+			//basic inputs
+			if (Input.GetKey(KeyCode.W))
+			{
+				playerRigid.AddForce(new Vector2(0, moveSpeed * 1000.0f));
+			}
+			if (Input.GetKey(KeyCode.A))
+			{
+				playerRigid.AddForce(new Vector2(-moveSpeed * 1000.0f, 0));
+			}
+			if (Input.GetKey(KeyCode.S))
+			{
+				playerRigid.AddForce(new Vector2(0, -moveSpeed * 1000.0f));
+			}
+			if (Input.GetKey(KeyCode.D))
+			{
+				playerRigid.AddForce(new Vector2(moveSpeed * 1000.0f, 0));
+			}
+		}
+	}
+}
---
+++ Global Game Jam 2019/Assets/Scripts/Tether.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tether : MonoBehaviour
+{
+
+	public float minDist = 2.0f;
+	public float maxDist = 4.0f;
+	public float moveSpeed = 1.6f;
+
+	private GameObject target;
+	private GameObject self;
+	private Rigidbody2D localRigid;
+	private Vector2 dir;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		target = GameObject.FindGameObjectWithTag("Player");
+		self = GameObject.FindGameObjectWithTag("House");
+		localRigid = self.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		dir = target.GetComponent<Transform>().position - self.GetComponent<Transform>().position;
+		dir.Normalize();
+    }
+
+	private void FixedUpdate()
+	{	//moves if out of min distance
+		if ((self.GetComponent<Transform>().position - target.GetComponent<Transform>().position).magnitude > minDist) {
+			localRigid.AddForce(moveSpeed*500.0f*dir);
+		}
+		//keeps up with player max range
+		if ((self.GetComponent<Transform>().position - target.GetComponent<Transform>().position).magnitude > maxDist) {
+			self.GetComponent<Transform>().position = target.GetComponent<Transform>().position - new Vector3((dir * maxDist).x, (dir * maxDist).y, 0.0f);
+		}
+
+	}
+}
