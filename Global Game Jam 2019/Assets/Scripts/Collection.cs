e7d5ba03a89f4f94aa216addee42271fc27aa6f6
Gil Robern <gil.robern@uoit.net>
2019-01-27 10:12:32 -0500

    Progress Bar WIP

--- Global Game Jam 2019/Assets/Scripts/Collection.cs
+++ Global Game Jam 2019/Assets/Scripts/Collection.cs
@@ -1,14 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 
 
 public class Collection : MonoBehaviour
 {
-	public int totalPoints;
+	public float totalPoints;
 	public List<int> collection;
 	public AudioSource source;
+	public Image meter;
 
     // Start is called before the first frame update
     void Start()
@@ -18,7 +20,7 @@ public class Collection : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+		meter.fillAmount = totalPoints / 25;
     }
 
 	private void OnTriggerEnter2D(Collider2D collision)
@@ -28,7 +30,8 @@ public class Collection : MonoBehaviour
 
 			RewardData data = collision.gameObject.GetComponent<RewardData>();
 			totalPoints += data.points;
-			collection.Add(data.index);	
+			collection.Add(data.index);
+			Debug.Log(totalPoints);
 			Debug.Log("Happening/n");
 			//collection anim goes here ... @anim
 			
