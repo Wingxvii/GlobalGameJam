853c0619567f55e8c1b4809e09763d30b894a331
John Wang <john.wang@uoit.net>
2019-01-27 12:40:47 -0500

    Added Monster Spawner

---
+++ Global Game Jam 2019/Assets/Scripts/SpawnLocation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnLocation : MonoBehaviour
+{
+
+	public Vector3 pos;
+	public bool isOutRange;
+	private Transform player;
+	public float range = 4.0f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		pos = this.GetComponent<Transform>().position;
+		player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+	private void Update()
+	{
+		if ((pos - player.position).magnitude > range)
+		{
+
+			isOutRange = true;
+		}
+		else {
+			isOutRange = false;
+		}
+	}
+}
---
+++ Global Game Jam 2019/Assets/Scripts/Spawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+	public GameObject monster;
+	public GameObject[] spawnLocations;
+
+	public int spawnFrames = 1000;
+	public int actualFrames;
+	public int rampDown = 50;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		actualFrames = spawnFrames;
+		spawnLocations = GameObject.FindGameObjectsWithTag("Spawner");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		actualFrames--;
+		if (actualFrames <= 0) {
+			foreach (GameObject obj in spawnLocations) {
+				if (obj.GetComponent<SpawnLocation>().isOutRange) {
+					Debug.Log("Spawn");
+					Instantiate(monster, obj.GetComponent<SpawnLocation>().pos, Quaternion.identity);
+				}
+			}
+
+			actualFrames = spawnFrames;
+			spawnFrames -= rampDown;
+		}
+
+    }
+}
