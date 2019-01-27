518ab4dab043c2af474ce1ff44c150a38b8c1594
Nawaf Bahadur <nawaf.bahadur@gmail.com>
2019-01-27 12:54:24 -0500

    Animations started, Char have sprites

---
+++ Global Game Jam 2019/Assets/Scripts/GeneralAnimation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneralAnimation : MonoBehaviour
+{
+
+
+    Animator localAnimator;
+    Rigidbody2D localRigid2D;
+
+
+    void Start()
+    {
+        localAnimator = GetComponentInChildren<Animator>();
+        localRigid2D = this.gameObject.GetComponent<Rigidbody2D>();
+
+    }
+
+    void Update()
+    {
+        
+
+    }
+}
